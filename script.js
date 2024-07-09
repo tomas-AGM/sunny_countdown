87cedb90c1922d2345f41658b22a9052f4ca4fe9
tomas-AGM <tomas.albiol.garcia.munte@gmail.com>
2024-07-09 15:17:42 +0200

    Updated script.js and style.css
    for smoother transition

--- script.js
+++ script.js
@@ -2,15 +2,18 @@ $(document).ready(function() {
     // Set the date we're counting down to
     var countDownDate = new Date("Dec 31, 2024 23:59:59").getTime();
 
-    // Get today's date and time
+    // Calculate the initial time difference
     var now = new Date().getTime();
-    
-    // Find the distance between now and the count down date
     var distance = countDownDate - now;
-
+    
     // Initialize the FlipClock
     var clock = $('.clock').FlipClock(distance / 1000, {
         clockFace: 'DailyCounter',
-        countdown: true
+        countdown: true,
+        callbacks: {
+            stop: function() {
+                $('.clock').html("EXPIRED");
+            }
+        }
     });
 });
